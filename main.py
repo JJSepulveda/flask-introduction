77935f0066d2c04d3a7b13d357a4d49a95fa8be5
Juan Jose <juan.sepulveda@edu.uag.mx>
2022-07-16 11:43:15 -0600

    hello world con request y response

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, make_response, redirect
+
+app = Flask(__name__)
+
+COOKIE_USER_IP = 'user_ip'
+
+@app.route('/')
+def index():
+	user_ip = request.remote_addr
+	response = make_response(redirect('/hello'))
+	response.set_cookie(COOKIE_USER_IP, user_ip)
+
+	return response
+
+
+@app.route('/hello')
+def hello():
+	user_ip = request.cookies.get(COOKIE_USER_IP)
+	return f'Hola, tu ip es: {user_ip}'
+
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=80, debug=True)
